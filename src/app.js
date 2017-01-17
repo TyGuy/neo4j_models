53c50629201fd638920a041def3c2f064eafef06
Tyler Davis <tee.s.davis@gmail.com>
2017-01-17 12:50:58 -0800

    first one

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import Butt from 'butt';
+
+let butt = new Butt('wise');
+butt.poop();
---
+++ src/butt.js
@@ -0,0 +1,7 @@
+export default class Butt extends Object {
+  constructor: =>(buttType) {
+    this.buttType = buttType;
+  },
+
+  poop: => { console.log('I am a ' + this.type + '-ass.')}
+}
