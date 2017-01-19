e95ac3a1263bb458087135367dff725385c1d98c
Tyler Davis <tee.s.davis@gmail.com>
2017-01-18 19:08:30 -0800

    Empty class an test for relationships

---
+++ test/models/neo_rel_test.js
@@ -0,0 +1,8 @@
+import NeoRel from '../../lib/models/neo_node'
+import Chai, { expect } from 'chai'
+import NeoDB from '../../lib/neo_db'
+import clearDB from '../helpers/clear_db'
+
+describe('NeoRel', () => {
+
+})
